4f2c70701f7769403abf941e8181ef63a0c6b161
Alcene <iradalcene@gmail.com>
2021-11-03 09:54:02 +0200

    Ajout des classes Forme

---
+++ Forme.cs
@@ -0,0 +1,8 @@
+using System;
+namespace ExercicePoo
+{
+    class Forme
+    {
+        public string couleur="Rouge";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ExercicePoo
+{
+    class Program
+    {
+        //public string nom="Christa";
+        static void Main(string[] args)
+        {
+            //Program p=new Program();
+           // Console.WriteLine(p.nom);
+           Forme f1=new Forme();
+            Console.WriteLine(f1.couleur);
+        }
+    }
+}
